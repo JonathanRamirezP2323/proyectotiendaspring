944ad0d95a99c9076a86f96fbd7a79fcbc6a8194
JR <jrperez@tecnologicoismac.edu.ec>
2026-06-19 13:23:00 -0500

    tienda spring

---
+++ src/main/java/com/tiendaapp/WhatsappNotificacion.java
@@ -0,0 +1,26 @@
+package com.tiendaapp;
+
+public class WhatsappNotificacion {
+
+    private String codigoPais;
+    private String numeroBase;
+
+    public void setCodigoPais(String p) {
+        this.codigoPais = p;
+    }
+
+    public void setNumeroBase(String b) {
+        this.numeroBase = b;
+    }
+
+    public void conectar() {
+        System.out.println("[WHATSAPP] Conectado al servidor (código: +593 ");
+    }
+
+    public void desconectar() {
+        System.out.println("[WHATSAPP] Desconectado del servidor (código: +593 ");
+    }
+    public void enviar(String mensaje) {
+        System.out.println("[WHATSAPP +593-0991234567] mensaje");
+    }
+}
